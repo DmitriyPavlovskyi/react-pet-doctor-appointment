865c0d90e7768db431193ae173669fae54554a7b
Dmitriy Pavlovskyi <pavlovskyidmitriy@gmail.com>
2017-05-12 15:47:16 +0300

    test

--- process/js/app.js
+++ process/js/app.js
@@ -2,7 +2,5 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var MainInterface = React.createClass({
-  render: function() {
-    return <h1>Pet Appointments</h1>
-  }
+
 });
